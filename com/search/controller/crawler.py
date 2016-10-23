de30dc2ea03eb263005904dc0cb54a2805b9deba
Ankur Rajput <rajputankur023@gmail.com>
2016-10-23 19:38:34 +0530

    indexing the web page and updated the crawl_web function accordingly

--- com/search/controller/crawler.py
+++ com/search/controller/crawler.py
@@ -26,12 +26,36 @@ def union(p, q):
             p.append(e)
 
 
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword, [url]])
+
+
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
+    index = []
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
             union(tocrawl, get_all_links(get_page(page)))
             crawled.append(page)
-    return crawled
+    return index
