4c157f35fd3126e8b013fdcc14225ee8e5a8231d
Ankur Rajput <rajputankur023@gmail.com>
2016-11-03 08:18:24 +0530

    using dictionary instead of list (for index) for faster results

--- com/search/controller/crawler.py
+++ com/search/controller/crawler.py
@@ -27,17 +27,19 @@ def union(p, q):
 
 
 def add_to_index(index, keyword, url):
-    for entry in index:
-        if entry[0] == keyword:
-            entry[1].append(url)
-            return
-    index.append([keyword, [url]])
+    if keyword in index:
+        if url not in index[keyword]:
+            index[keyword].append(url)
+
+    else:
+        #not found, add new keyword to index
+        index[keyword]=[url]
 
 
 def lookup(index, keyword):
-    for entry in index:
-        if entry[0] == keyword:
-            return entry[1]
+    if keyword in index:
+        return index[keyword]
+
     return []
 
 
@@ -58,12 +60,14 @@ def get_page(url):
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
-    index = []
+    index = {}
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             content = get_page(page)
             add_page_to_index(index, page, content)
-            union(tocrawl, get_all_links(get_page(page)))
+            union(tocrawl, get_all_links(content))
             crawled.append(page)
     return index
+
+#print crawl_web("http://www.google.co.in")
